d6c1ce5a0a2f631ceded68d7242ec9f9aa8855c3
Michael Schlueter <schluet0r4life@gmail.com>
2022-09-20 10:01:59 +0200

    retrieve all transactions

--- controller/transactions.js
+++ controller/transactions.js
@@ -1,1 +1,25 @@
 const { db } = require("../model/db");
+
+// @desc    Get all transactions
+// @route   GET /api/transactions
+const getAllTransactions = async (req, res) => {
+  const query = "SELECT * FROM transactions";
+  try {
+    const transactions = await db.query(query);
+    if (transactions.rowCount < 1) {
+      return res.status(404).send({
+        message: "No transactions found",
+      });
+    }
+
+    return res.status(200).send(transactions.rows);
+  } catch (err) {
+    return res.status(500).send({
+      error: err.message,
+    });
+  }
+};
+
+module.exports = {
+  getAllTransactions,
+};
--- routes/transactions.js
+++ routes/transactions.js
@@ -2,18 +2,18 @@ const express = require("express");
 
 const {
   getAllTransactions,
-  getTransaction,
-  createTransaction,
-  updateTransaction,
-  deleteTransaction,
-} = require("../controller/envelopes");
+//   getTransaction,
+//   createTransaction,
+//   updateTransaction,
+//   deleteTransaction,
+} = require("../controller/transactions");
 
 const transactionsRouter = express.Router();
 
 transactionsRouter.get("/", getAllTransactions);
-transactionsRouter.get("/:id", getTransaction);
-transactionsRouter.post("/", createTransaction);
-transactionsRouter.put("/:id", updateTransaction);
-transactionsRouter.delete("/:id", deleteTransaction);
+// transactionsRouter.get("/:id", getTransaction);
+// transactionsRouter.post("/", createTransaction);
+// transactionsRouter.put("/:id", updateTransaction);
+// transactionsRouter.delete("/:id", deleteTransaction);
 
 module.exports = transactionsRouter;
