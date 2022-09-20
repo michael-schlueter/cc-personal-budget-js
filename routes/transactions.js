c730905d5aa41d26907ee0fa2f843f1d12c54912
Michael Schlueter <schluet0r4life@gmail.com>
2022-09-20 09:50:31 +0200

    add routing for transactions

---
+++ routes/transactions.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const {
+  getAllTransactions,
+  getTransaction,
+  createTransaction,
+  updateTransaction,
+  deleteTransaction,
+} = require("../controller/envelopes");
+
+const transactionsRouter = express.Router();
+
+transactionsRouter.get("/", getAllTransactions);
+transactionsRouter.get("/:id", getTransaction);
+transactionsRouter.post("/", createTransaction);
+transactionsRouter.put("/:id", updateTransaction);
+transactionsRouter.delete("/:id", deleteTransaction);
+
+module.exports = transactionsRouter;
